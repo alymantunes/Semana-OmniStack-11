0fc22b2f4aefa215e2c8714646add5b0b8c20489
Alym Antunes <alymab@gmail.com>
2020-03-25 12:59:17 -0300

    Limpeza codigo, definição de "estado"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from 'react';
 
 function App() {
+  const [counter,setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniStack 11
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>counter: {counter}</h1>
+      <button onClick={increment}>increment</button>
     </div>
   );
 }
